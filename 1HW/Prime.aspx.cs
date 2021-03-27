8f7ed4d8462f29378e2841e60c156e36e79cda60
傳儒 鄭 <107111216@mail.oit.edu.tw>
2021-03-27 20:55:22 +0800

    0327

---
+++ 1HW/Prime.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1HW
+{
+    public partial class Prime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int prime = 80;
+            bool j = true;
+            if (prime == 1)
+            {
+                Response.Write("NO");
+            }
+            else if (prime == 2)
+            {
+                Response.Write("Yes");
+            }
+            else if (prime > 2)
+            {
+                for (int i = 2; i < prime; i++)
+                {
+                    if (prime % i == 0)
+                    {
+                        Response.Write("NO");
+                        j = false;
+                        break;
+                    }
+                }
+                if (j == true)
+                    Response.Write("Yes");
+            }
+        }
+    }
+}
